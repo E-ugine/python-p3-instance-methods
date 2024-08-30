4f0fb0251063667fb541f2cccd2480ea096d3754
EugineAgolla <agollaeugine@gmail.com>
2024-08-30 12:30:06 +0300

    instance methods OOP

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,16 @@
 
 class Dog:
     # Class body goes here
-
+    def _init_(self,name,bark):
+        self.name
+        self.bark
+    
     #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
+
+dog = Dog()
+dog.sit()
+dog.bark()
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
+    def _init_(self,talk,name):
+        self.name
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")  
+
+person = Person()
+person.talk()
+person.walk()
